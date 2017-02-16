4bc5ae3d20cf9107d90c9bd19d09326990df30f1
Marina Torras <mtorras@ucsd.edu>
2017-02-16 11:55:48 -0800

    lab6 done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -20,13 +20,21 @@ function initializePage() {
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
-
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get('/project/' + idNumber, addProject);
 	console.log("User clicked on project " + idNumber);
+}
+
+//Empty callback function
+function addProject(result){
+	console.log(result);
+	var projectHTML = '<img src="' + result['image'] 
+	+ '" class ="img detailsImage">' + '<p><small>' + result['date'] + '</p></small>' + result['summary']
+	$('#project' + result['id'] + ' .details').html(projectHTML);
 }
 
 /*
@@ -35,4 +43,11 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette', function (colors) {
+    $('body').css('background-color', colors[0]);
+    $('.thumbnail').css('background-color', colors[1]);
+    $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+    $('p').css('color', colors[3]);
+    $('.project img').css('opacity', .75);
+  });
 }
